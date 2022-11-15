881b9a8bc0f32bc9e48523c372451b6780128eb6
brandon <brandonsurh@gmail.com>
2022-11-14 19:27:41 -0500

    Created basic frontend. Need to deploy for contract ID.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Wallet } from "fuels";
+import "./App.css";
+// Import the contract factory -- you can find the name in index.ts.
+// You can also do command + space and the compiler will suggest the correct name.
+import { RsvpContractAbi__factory } from "./contracts";
+// The address of the contract deployed the Fuel testnet
+const CONTRACT_ID = "<need funds to deploy>";
+//the private key from createWallet.js
+const WALLET_SECRET = "0xba4008117ba7d1f7cdd817678a063ca5751ead6c7d290e6aa74d6b3c031eae21"
+// Create a Wallet from given secretKey in this case
+// The one we configured at the chainConfig.json
+const wallet = new Wallet(WALLET_SECRET, "https://node-beta-1.fuel.network/graphql");
+// Connects out Contract instance to the deployed contract
+// address using the given wallet.
+const contract = Abi__factory.connect(CONTRACT_ID, wallet);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const [loading, setLoading] = useState(false);
+  const [eventId, setEventId] = useState('');
+  const [eventName, setEventName] = useState('')
+  const [maxCap, setMaxCap] = useState(0)
+  const [deposit, setDeposit] = useState(0)
+  const [eventCreation, setEventCreation] = useState(false);
+  const [rsvpConfirmed, setRSVPConfirmed] = useState(false);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log("eventName", eventName);
+    console.log("deposit", deposit);
+    console.log("max cap", maxCap);
+  },[eventName, maxCap, deposit]);
+
+  async function rsvpToEvent(){
+    setLoading(true);
+    try {
+      const { value } = await contract.functions.rsvp(eventId).txParams({gasPrice: 1}).call();
+      console.log("RSVP'd to the following event", value);
+      console.log("deposit value", value.deposit.toString());
+      setEventName(value.name.toString());
+      setEventId(value.uniqueId.toString());
+      setMaxCap(value.maxCapacity.toNumber());
+      setDeposit(value.deposit.toNumber());
+      console.log("event name", value.name);
+      console.log("event capacity", value.maxCapacity.toString());
+      console.log("eventID", value.uniqueId.toString()) 
+      setRSVPConfirmed(true);
+      alert("rsvp successful")
+    } catch (err: any) {
+      alert(err.message);
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  async function createEvent(e: any){
+    e.preventDefault();
+    setLoading(true);
+    try {
+      console.log("creating event")
+      const { value } = await contract.functions.create_event(maxCap, deposit, eventName).txParams({gasPrice: 1}).call();
+
+      console.log("return of create event", value);
+      console.log("deposit value", value.deposit.toString());
+      console.log("event name", value.name);
+      console.log("event capacity", value.maxCapacity.toString());
+      console.log("eventID", value.uniqueId.toString())
+      setEventId(value.uniqueId.toString())
+      setEventCreation(true);
+      alert('Event created');
+    } catch (err: any) {
+      alert(err.message)
+    } finally {
+      setLoading(false);
+    }
+  }
+return (
+  <div>
+    <form id="createEventForm" onSubmit={createEvent}>
+    <input value = {eventName} onChange={e => setEventName(e.target.value) }name="eventName" type="text" placeholder="Enter event name" />
+      <input value = {maxCap} onChange={e => setMaxCap(+e.target.value)} name="maxCapacity" type="text" placeholder="Enter max capacity" />
+      <input value = {deposit} onChange={e => setDeposit(+e.target.value)} name="price" type="number" placeholder="Enter price" />
+      <button disabled={loading}>
+        {loading ? "creating..." : "create"}
+      </button>
+    </form>
+    <div>
+      <input name="eventId" onChange={e => setEventId(e.target.value)} placeholder="pass in the eventID"/>
+      <button onClick={rsvpToEvent}>RSVP</button>
     </div>
-  );
+    <div> 
+    {eventCreation &&
+    <>
+    <h1> New event created</h1>
+    <h2> Event Name: {eventName} </h2>
+    <h2> Event ID: {eventId}</h2>
+    <h2>Max capacity: {maxCap}</h2>
+    <h2>Deposit: {deposit}</h2>
+    </>
+    }
+    </div> 
+    <div>
+    {rsvpConfirmed && <>
+    <h1>RSVP Confirmed to the following event: {eventName}</h1>
+    </>}
+    </div>
+  </div>
+);
 }
-
-export default App;
